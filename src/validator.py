36f9841a4ee7196747c3cdd2746c52c5cdefddd0
Fernando Terrones <terronesfernando453@gmail.com>
2026-03-27 18:18:25 -0400

    Add is_positive helper function.

--- src/validator.py
+++ src/validator.py
@@ -1,5 +1,5 @@
 """Module provides functions to check if input is  valid"""
-
+##Hello
 def validate_number(value):
     """Validate that value can be converted to a number."""
     try:
@@ -20,3 +20,6 @@ def validate_positive(n):
         return num > 0
     except (ValueError, TypeError):
         return False
+def is_positive(n):
+    """Check if a number is positive."""
+    return n > 0
